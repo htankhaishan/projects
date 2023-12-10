dbc6536c75ce8913edd5c7a37bba7eb65323e19a
Kyaw Htet Thu <htankhaishan@gmail.com>
2023-12-10 23:50:38 +0800

    Final Update

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className="flex justify-end space-x-2">
             <a href="#projects" className="p-4 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400">Projects</a>
             <a href="#aboutMe" className="p-4 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400">About</a>
-            <a href="#contact" className="p-4 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400">Download CV</a>
+            <a href="https://drive.google.com/file/d/10Rg7DKsVAPhdkpqiFZ30Q25hYL2BBYiZ/view?usp=drive_link" className="p-4 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400">Download CV</a>
             </div>
         </div>
     </nav>
