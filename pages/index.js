2bac0c47e7601278f0847321fd6fbf198092b267
Kyaw Htet Thu <htankhaishan@gmail.com>
2023-12-11 12:50:45 +0800

    URLlogoUpdate

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import React from 'react';
 import NavBar from '../components/NavBar';
 import AboutMe from '../components/AboutMe';
@@ -16,6 +17,10 @@ const HeroSection = dynamic(
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col ">
+        <Head>
+          <link rel="icon" href="/logo.png" />
+          <title>Space INU</title>
+        </Head>
         <main>
         <NavBar />
         <HeroSection />
